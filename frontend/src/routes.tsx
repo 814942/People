bc651fb6e2cd681804c1e5ba1f5d2064922b8f28
Pablo Garay <elxebec@gmail.com>
2022-06-14 13:06:01 -0300

    Deleted Not Found component

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,14 +3,12 @@ import { Router } from "@reach/router";
 
 import { Layout } from './layout/layout.component';
 import { DashboardPage } from './pages/dashboard.page';
-import { NotFound } from "./components/notFound/not-found";
 
 export function Routes() {
   return (
     <Layout>
       <Router>
-        <DashboardPage path="/dashboard"/>
-        <NotFound default />
+        <DashboardPage default path="/dashboard"/>
       </Router>
     </Layout>
   );
