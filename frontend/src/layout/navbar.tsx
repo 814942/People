2876b89d14147f25fafed06bf457a474f235b030
Pablo Garay <elxebec@gmail.com>
2022-06-15 23:00:54 -0300

    Clean up

--- frontend/src/layout/navbar.tsx
+++ frontend/src/layout/navbar.tsx
@@ -6,11 +6,9 @@ import { RouteComponentProps } from "@reach/router";
 export const NavBar: React.FC<RouteComponentProps> = () => {
   return (
     <AppBar position="fixed">
-      <div style={{ display: "flex", justifyContent: "center" }}>
       <Button color="inherit" href="/dashboard">
         People's Dashboard
       </Button>
-      </div>
     </AppBar>
   );
 };
--- frontend/src/pages/dashboard.page.tsx
+++ frontend/src/pages/dashboard.page.tsx
@@ -5,7 +5,5 @@ import { RouteComponentProps } from "@reach/router";
 type TParams = { id: string };
 
 export const DashboardPage: FC<RouteComponentProps> = (props: RouteComponentProps<TParams>) => {
-  return (
-    <div style={{ padding: "5rem", textAlign: "center", color: "white" }}>Dashboard</div>
-  )
+  return <></>
 };
